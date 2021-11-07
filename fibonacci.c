8d023ccfcf6c3340801b0195de0afa1f93d03e94
sivaraj1908 <93810828+sivaraj1908@users.noreply.github.com>
2021-11-07 10:59:55 +0530

    Add files via upload

--- fibonacci.c
+++ fibonacci.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <conio.h>
 
+
 void main()
 {
-    int i,a=0,b=1,c;
+     long long int i,a=0,b=1,c;
     for(i=2;i<100;i++){
         c=a+b;
         a=b;
         b=c;
     }
-      printf("The %dth fibonacci series = %d\n   ",i,c);
+      printf("The %dth fibonacci series = %llu\n   ",i,c);
 }
